52f660016f3ee9b7677cb1c3866458a63bfad4be
TWMKyle <ktrrsnf3@gmail.com>
2026-08-18 20:48:21 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -74,7 +74,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 def run_kds_music():
     service_list = ["10AM", "12NN", "2PM", "4PM", "6PM"]
     week_list = ["Week1", "Week2", "Week3", "Week4", "Week5"]
-    role_list = ["AG", "WL"]
+    role_list = ["AG", "WL", "MusicBackup"]
     month_list = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
@@ -197,7 +197,7 @@ def run_kds_music():
 def run_kds_teacher():
     service_list2 = ["10AM", "12NN", "2PM", "4PM", "6PM"]
     week_list2 = ["Week1", "Week2", "Week3", "Week4", "Week5"]
-    role_list2 = ["Volunteer", "Preacher"]
+    role_list2 = ["Volunteer", "Preacher", "TeacherBackup"]
     month_list2 = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
