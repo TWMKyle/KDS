3ee0e79074a6c88eb71dd6a654abf37a194089e1
TWMKyle <ktrrsnf3@gmail.com>
2026-08-18 17:14:29 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -283,15 +283,15 @@ def run_kds_teacher():
                 m_age2 = df_latest2["Agegroup"].fillna("").astype(str).str.strip().str.lower() == age_term2.lower()
 
                 duplicate_collision2 = (m_name2 & m_srv2 & m_wk2 & m_role2 & m_mnt2 & m_age2).any()
-                duplicate_preacher2 = (m_name2 & m_srv2 & m_wk2 & m_role2 & m_mnt2 & m_age2).any()
                 matching_slots2 = df_latest2[m_srv2 & m_wk2 & m_role2 & m_mnt2]
-                duplicate_service2 = (m_srv2 & m_wk2 & m_role2 & m_mnt2).any()
+                duplicate_service2 = (m_srv2 & m_wk2 & m_mnt2).any()
 
                 if duplicate_collision2:
                     st.error(
                         f"Duplicate Error: You are already serving for the {srv_term2} on {wk_term2} as a/an {rl_term2} in {mnt_term2}!")
 
                 elif duplicate_service2:
+                    
                     st.error(
                         f"Oops, someone is already serving for the {srv_term2} on {wk_term2} as a/an {rl_term2} in {mnt_term2} for {age_term2}!")
                     conflicting_row2 = matching_slots2[required_columns2].copy()
