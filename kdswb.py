deb43d9a0cb26527c66d300323b49c82816cffe7
TWMKyle <ktrrsnf3@gmail.com>
2026-08-18 14:55:16 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -448,10 +448,11 @@ st.html(
         text-align: center;
         background-color: #f9f9f9;
     ">
-        <img src="https://unsplash.com" 
+        <img src="https://github.com/TWMKyle/KDS/blob/main/428153935_7645976362101063_1868470333701431125_n.jpg" 
              style="width: 100%; border-radius: 5%; margin-bottom: 10px;">
         <h3 style="margin: 0; color: #333; font-family: sans-serif; font-size: 18px;">
-            Jane Doe
+            John Venn
+            12 Noon - Worship Service Leader
         </h3>
     </div>
     """
