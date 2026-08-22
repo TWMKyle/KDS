53037ac5c57a4babc995f3ee3c2e8f04639deb35
TWMKyle <ktrrsnf3@gmail.com>
2026-08-22 13:41:01 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -543,7 +543,7 @@ with tab2:
         teacherteamweek_lookup()
 
 with tab3:
-    st.subheader("If you have any questions or concerns, you can reach out to the following coordinators: ")
+    st.write("If you have any questions or concerns, you can reach out to the following coordinators: ")
 
     image_jv = "428153935_7645976362101063_1868470333701431125_n.jpg"
     image_kyle = "images-2.jpeg"
