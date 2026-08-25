aaab839cadd71a9d5e2f909c19473a5fa429b149
TWMKyle <ktrrsnf3@gmail.com>
2026-08-25 13:15:42 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -10,8 +10,8 @@ from PIL import Image
 
 
 
-#img_srcx = "https://raw.githubusercontent.com/TWMKyle/KDS/main/360_F_785703636_rd7q7qqRbtrJAnUrslc5yqbNlq2eDX4y.jpg"
-img_srcy = "https://raw.githubusercontent.com/TWMKyle/twm/main/a38d642b-d7f2-4989-91bc-13fd2048683c.jpg"
+img_srcx = "https://raw.githubusercontent.com/TWMKyle/KDS/main/360_F_785703636_rd7q7qqRbtrJAnUrslc5yqbNlq2eDX4y.jpg"
+#img_srcy = "https://raw.githubusercontent.com/TWMKyle/twm/main/a38d642b-d7f2-4989-91bc-13fd2048683c.jpg"
 
 # 2. Inject styling to target the ENTIRE Streamlit app frame
 st.markdown(
@@ -19,7 +19,7 @@ st.markdown(
     <style>
     /* This selector targets the entire background canvas of Streamlit */
     [data-testid="stAppViewContainer"] {{
-        background-image: url('{img_srcy}') !important;
+        background-image: url('{img_srcx}') !important;
         background-size: cover !important;
         background-position: center !important;
         background-repeat: no-repeat !important;
