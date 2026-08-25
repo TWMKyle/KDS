a7390e30d93edcfb7d5517c4843061ffd2603ec9
TWMKyle <ktrrsnf3@gmail.com>
2026-08-25 22:29:37 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -51,7 +51,7 @@ st.markdown(
     [data-testid="stTabs"] [role="tablist"] p,
     .stTabs [role="tab"] p,
     [data-testid="stTabs"] button[role="tab"] p {{
-        color: #FFFFFF !important; 
+        color: #000000 !important; 
     }}
 
     [data-testid="stViewerBadge"],
