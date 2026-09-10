e9c1543d68207068cc59ada1c4d303069d81b11e
TWMKyle <ktrrsnf3@gmail.com>
2026-09-10 19:48:20 +0800

    Update kdswb.py

--- kdswb.py
+++ kdswb.py
@@ -810,6 +810,30 @@ with tab3:
         """
 
         f"""
+         
+        <div style="
+            border: 2px solid #4A90E2;
+            border-radius: 10px;
+            padding: 15px;
+            width: 180px;
+            text-align: center;
+            background-color: #f9f9f9;
+            font-family: sans-serif;
+        ">
+            <img src="{img_src1}" 
+                 style="width: 100%; border-radius: 5%; margin-bottom: 10px; object-fit: cover;">
+
+            <h2 style="margin: 0 0 5px 0; color: #666; font-size: 18px; font-weight: bold;">
+                John Venn
+            </h2>
+            <p style="margin: 0; color: #666; font-size: 13px; line-height: 1.3;">
+                Worship Service Leader
+                12NN
+            </p>
+        </div>
+        """
+
+        f"""
         <div style="
             border: 2px solid #4A90E2;
             border-radius: 10px;
